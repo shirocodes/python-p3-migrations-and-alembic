f9b5c7d2eadbcd49beeda9a69b02a5a6335db99b
shirocodes <ishirapt@gmail.com>
2025-05-23 16:07:58 +0300

    managed migrations.

---
+++ lib/migrations/versions/1d2e009ddcaa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1d2e009ddcaa
+Revises: 
+Create Date: 2025-05-23 15:34:23.795685
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1d2e009ddcaa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
